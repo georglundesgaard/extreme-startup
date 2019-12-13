fc5fbf6b04066ab5d3990caf8c250d2fa86c0982
Georg Sirnes Lundesgaard <georg@lundesgaard.no>
2019-12-13 13:01:14 +0100

    GH-1 intial question-service

---
+++ question-service/src/test/kotlin/no/lundesgaard/extremestartup/question/QuestionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.lundesgaard.extremestartup.question
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
