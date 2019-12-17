c7ab5b13ef73a5f2401cce2c548e47f88003b888
Georg Sirnes Lundesgaard <georg@lundesgaard.no>
2019-12-17 22:16:50 +0100

    GH-1 question-service with more questions

--- question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/QuestionController.kt
+++ question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/QuestionController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.lang.String.format
@@ -13,37 +13,47 @@ import kotlin.random.Random.Default.nextInt
 
 @RestController
 class QuestionController {
-    private val questions: HashMap<String, Question<Int>> = HashMap() 
+    private val questionMap = questionMap()
+    private val questions: HashMap<String, Question> = HashMap()
+    
+    @GetMapping("/types")
+    fun types(): String = format("%s\n", questionMap.keys.joinToString())
     
     @GetMapping("/random")
     fun randomQuestion(): String {
-        val r = nextInt(100)
-        val q = if (r < 33) AdditionQuestion() else if (r < 66) SubtractionQuestion() else MultiplicationQuestion()
+        val i = nextInt(questionMap.size)
+        val q = questionMap.values.toList()[i].call()
+        return processQuestion(q)
+    }
+
+    @GetMapping("/warmup")
+    fun warmupQuestion(@RequestParam(required = false, defaultValue = "unknown") p: String): String {
+        val q = WarmupQuestion(p)
+        return processQuestion(q)
+    }
+    
+    @GetMapping("/{idOrType}")
+    fun question(@PathVariable idOrType: String): String =
+            processQuestion(questions[idOrType] ?: questionMap[idOrType]?.call() ?: throw MissingIdOrTypeException(idOrType))
+
+    private fun processQuestion(q: Question): String {
         questions[q.id] = q
         return format("%s\n", q)
     }
-    
-    @GetMapping("/{id}")
-    fun question(@PathVariable id: String): String {
-        val q = questions[id] ?: throw MissingQuestionException(id)
-        return format("%s\n", q)
-    }
-    
+
     @PostMapping("/{id}")
-    fun checkAnswer(@PathVariable id: String, @RequestBody answer: String): String {
-        val q = questions[id] ?: throw MissingQuestionException(id)
-        return format("%s\n", answer == q.correctAnswer().toString())
-    }
+    fun checkAnswer(@PathVariable id: String, @RequestParam(required = false, defaultValue = "") a: String): String =
+            format("%s\n", questions[id]?.checkAnswer(a) ?: throw MissingIdException(id))
     
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: String):String {
-        val q = questions.remove(id)
-        return format("%s\n", q?.id ?: "")
-    }
+    fun delete(@PathVariable id: String): String = format("%s\n", questions.remove(id)?.id ?: "")
     
     @GetMapping("/")
-    fun questions(): String = format("%d\n", questions.size)
+    fun questionCount(): String = format("%d\n", questions.size)
 }
 
 @ResponseStatus(NOT_FOUND)
-class MissingQuestionException(id: String): RuntimeException(format("Unknown question id: %s", id))
+class MissingIdOrTypeException(idOrType: String): RuntimeException(format("Unknown question id or type: %s", idOrType))
+
+@ResponseStatus(NOT_FOUND)
+class MissingIdException(id: String): RuntimeException(format("Unknown question id: %s", id))
