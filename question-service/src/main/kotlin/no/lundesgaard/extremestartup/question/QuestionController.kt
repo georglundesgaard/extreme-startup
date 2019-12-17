2e43a8f65ba495a4bda811655f586e7f491e9df9
Georg Sirnes Lundesgaard <georg@lundesgaard.no>
2019-12-17 10:37:31 +0100

    GH-1 question-service with random questions and check answer functionality

--- question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/QuestionController.kt
+++ question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/QuestionController.kt
@@ -1,16 +1,49 @@
 package no.lundesgaard.extremestartup.question
 
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.lang.String.format
 import kotlin.random.Random.Default.nextInt
 
 @RestController
 class QuestionController {
-    @GetMapping("/")
-    fun getQuestionSummary(): String {
+    private val questions: HashMap<String, Question<Int>> = HashMap() 
+    
+    @GetMapping("/random")
+    fun randomQuestion(): String {
         val r = nextInt(100)
         val q = if (r < 33) AdditionQuestion() else if (r < 66) SubtractionQuestion() else MultiplicationQuestion()
-        return format("%s: %s? %s\n", q.id, q, q.correctAnswer())
+        questions[q.id] = q
+        return format("%s\n", q)
     }
-} 
+    
+    @GetMapping("/{id}")
+    fun question(@PathVariable id: String): String {
+        val q = questions[id] ?: throw MissingQuestionException(id)
+        return format("%s\n", q)
+    }
+    
+    @PostMapping("/{id}")
+    fun checkAnswer(@PathVariable id: String, @RequestBody answer: String): String {
+        val q = questions[id] ?: throw MissingQuestionException(id)
+        return format("%s\n", answer == q.correctAnswer().toString())
+    }
+    
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: String):String {
+        val q = questions.remove(id)
+        return format("%s\n", q?.id ?: "")
+    }
+    
+    @GetMapping("/")
+    fun questions(): String = format("%d\n", questions.size)
+}
+
+@ResponseStatus(NOT_FOUND)
+class MissingQuestionException(id: String): RuntimeException(format("Unknown question id: %s", id))
--- question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/questions.kt
+++ question-service/src/main/kotlin/no/lundesgaard/extremestartup/question/questions.kt
@@ -6,22 +6,24 @@ import kotlin.random.Random.Default.nextInt
 fun questionId() = UUID.randomUUID().toString().substring(0, 8)
 
 abstract class Question<A>(val id: String = questionId()) {
+    abstract fun asText(): String
     abstract fun correctAnswer(): A
+    override fun toString() = "%s: %s".format(id, asText())
 }
 
 abstract class BinaryMathsQuestion(val n1: Int = nextInt(20), val n2: Int = nextInt(20)): Question<Int>()
 
 class AdditionQuestion: BinaryMathsQuestion() {
-    override fun toString() = "what is %d plus %d".format(n1, n2)
+    override fun asText() = "what is %d plus %d".format(n1, n2)
     override fun correctAnswer() = n1 + n2
 }
 
 class SubtractionQuestion: BinaryMathsQuestion() {
-    override fun toString() = "what is %d minus %d".format(n1, n2)
+    override fun asText() = "what is %d minus %d".format(n1, n2)
     override fun correctAnswer() = n1 - n2
 }
 
 class MultiplicationQuestion: BinaryMathsQuestion() {
-    override fun toString() = "what is %d multiplied by %d".format(n1, n2)
+    override fun asText() = "what is %d multiplied by %d".format(n1, n2)
     override fun correctAnswer() = n1 * n2 
 }
